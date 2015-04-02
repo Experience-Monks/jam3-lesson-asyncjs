6d80cf6595bdb2b47113cd61ae0f94bb0e51b464
Mikko Haapoja <me@mikkoh.com>
2015-04-02 16:50:24 -0400

    Finished off practice #1

--- lessons/1-what is asynchronous js/solution/index.js
+++ lessons/1-what is asynchronous js/solution/index.js
@@ -11,5 +11,7 @@ module.exports = function(button, onTestRollOver, onTestRollOut, onTestClick) {
 	// 3. Remove onTestRollOut listener from button. It's bound to the 'mouseout' event.
 	// 4. Go and click the button ;)
 	
-	
+	button.addEventListener('mouseover', onTestRollOver);
+	button.addEventListener('click', onTestClick);
+	button.removeEventListener('mouseout', onTestRollOut);
 };
